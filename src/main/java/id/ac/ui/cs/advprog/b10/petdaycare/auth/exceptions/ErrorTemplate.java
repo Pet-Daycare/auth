efb12294a49ab3fa5dbba86d241e5f0c0ccd08a5
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-05-23 01:02:41 +0700

    improve exceptions for testing

--- src/main/java/id/ac/ui/cs/advprog/b10/petdaycare/auth/exceptions/ErrorTemplate.java
+++ src/main/java/id/ac/ui/cs/advprog/b10/petdaycare/auth/exceptions/ErrorTemplate.java
@@ -5,4 +5,11 @@ import org.springframework.http.HttpStatus;
 import java.time.ZonedDateTime;
 
 public record ErrorTemplate(String message, HttpStatus httpStatus, ZonedDateTime timestamp) {
+    public HttpStatus getStatus() {
+        return this.httpStatus;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
