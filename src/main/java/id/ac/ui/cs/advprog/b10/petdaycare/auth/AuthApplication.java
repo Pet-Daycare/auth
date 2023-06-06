8eaae9701ec436cb3b59a24dae6fa3773921ea18
achievagemilang <achievafuturagemilang@gmail.com>
2023-06-06 15:30:42 +0700

    deploy fix security disable

--- src/main/java/id/ac/ui/cs/advprog/b10/petdaycare/auth/AuthApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/b10/petdaycare/auth/AuthApplication.java
@@ -1,9 +1,17 @@
 package id.ac.ui.cs.advprog.b10.petdaycare.auth;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class,
+		ReactiveSecurityAutoConfiguration.class,
+		ReactiveManagementWebSecurityAutoConfiguration.class,
+}
+)
 public class AuthApplication {
 
 	public static void main(String[] args) {
